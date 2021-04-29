2fe2a58066ec20fdd85f14499b08f2176e4972e4
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-29 12:25:48 +0530

    Update router_auth.service.ts

--- src/app/services/router_auth.service.ts
+++ src/app/services/router_auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot,RouterStateSnapshot, CanActivateChild, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
  
  
@@ -9,19 +8,18 @@ export class RouterAuthService implements CanActivateChild {
  
     constructor(private _router:Router, private loginService: LoginService ) {
     }
-    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if (!this.loginService.isUserLoggedIn()) {
-            alert('You are not allowed to view this page');
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+        if (!this.loginService.isAdmin()) {
+            alert('You are not allowed to view this child page. Try using admin login');
             return false;
         }
+        return true;
     }
  
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): boolean {
- 
-        //check some condition  
         if (!this.loginService.isUserLoggedIn())  {
-            alert('You are not allowed to view this page');
+            alert('You are not allowed to view this page. Try after login');
             return false;
         } 
         return true;
