3a7c354486f5c73f2514232ec6ca9b663837fcb1
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-29 12:25:03 +0530

    Update header-page.component.ts

--- src/app/header-page/header-page.component.ts
+++ src/app/header-page/header-page.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-header-page',
   templateUrl: './header-page.component.html'
 })
 export class HeaderPageComponent implements OnInit {
-
-  constructor() { }
+  isLoggedin: boolean; 
+  constructor(private router: Router, private loginService: LoginService) {
+    this.router.events.subscribe((event:Event) => {
+      if(event instanceof NavigationEnd ){
+        this.isLoggedin = this.loginService.isUserLoggedIn() != undefined && this.loginService.isUserLoggedIn() ? true  : false;
+      }
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  logout(){
+    this.loginService.logoutUser();
+  }
 }
