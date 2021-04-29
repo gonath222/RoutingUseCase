8e4dcc6aa49d1a213f54cd0981cd9c12d7ee115f
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-29 12:24:57 +0530

    Update dashboard-page.component.ts

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.css'],
-  providers:[LoginService]
+  styleUrls: ['./dashboard-page.component.css']
 })
 export class DashboardPageComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  idvalue: string;
+  
+  constructor(private loginService: LoginService, 
+    private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
