079512b454ba9b7b6d6593ede051e375d3b8275b
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-29 12:25:11 +0530

    Update login-page.component.ts

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -4,9 +4,7 @@ import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  providers:[LoginService]
-
+  templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent implements OnInit {
   isLoginClicked = false;
@@ -14,31 +12,22 @@ export class LoginPageComponent implements OnInit {
   password : string ="";
   usernameResult : string ="";
   passwordResult : string ="";
-  retUrl : string ="dashboard";
+
   constructor(private loginService: LoginService, 
-    private router: Router, 
-    private activatedRoute:ActivatedRoute) { }
+    private router: Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.queryParamMap
-    .subscribe(params => {
-          this.retUrl = params.get('retUrl'); 
-          console.log( 'LoginComponent/ngOnInit '+ this.retUrl);
-        });
+
   }
 
   OnSubmitLogin(){
+    debugger;
     this.isLoginClicked = true;
     this.usernameResult = this.username;
     this.passwordResult = this.password;
 
     this.loginService.login(this.username, this.password).subscribe(data => {
-      console.log( 'return to '+ this.retUrl);
-      if (this.retUrl!=null) {
-           this.router.navigate( [this.retUrl]);
-      } else {
-           this.router.navigate( ['home']);
-      }
+      this.router.navigate( ['dashboard']);
   });
   }
 
